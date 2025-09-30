96c0292bdfb31ba0a23bf9ad5ffb126f5fa7f5dc
Samu-uh06 <samuelurrego4@gmail.com>
2025-09-30 12:23:23 -0500

    version final

---
+++ src/application/use-cases/orderDetails/GetOrderDetailById.js
@@ -0,0 +1,8 @@
+export default class GetOrderDetailsById {
+    constructor(orderDetailsRepository) {
+        this.orderDetailsRepository = orderDetailsRepository;
+    }
+    async execute(id) {
+        return await this.orderDetailsRepository.findById(id);
+    }
+}
